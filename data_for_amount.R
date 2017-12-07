a6020afb311dd5891ba37c86298a26a5e1c72c99
Abhishek Suntwal <abhisheksuntwal@gmail.com>
2017-12-06 17:25:42 -0800

    updated

--- data_for_amount.R
+++ data_for_amount.R
@@ -3,7 +3,11 @@ library("tidyr")
 library(lubridate)
 library(Hmisc)
 
-#Use this file to generate files which will be used to predict PaymentID
+#Use this file to generate files which will be used to predict Total Amount Bucket
+#Removes "VendorID", "passenger_count", "fare_amount", "extra", "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge"
+#Since the total amount is directly dependent on these and we want to see if the corrxct proice range is predicted even these 
+#are not provideed and what are the changes if they are included.
+#We deleted the total amount column and introduced the Amount buckets for simplification
 #Reading the file in to dataframe and filtering the data based on Columbia University area Morning Heights
 dataframe <- read.csv('C:/Users/abhishek.suntwal/Downloads/yellow_tripdata_2015-09.csv', header = TRUE)
 dataframe <- dataframe[(dataframe$pickup_latitude <= 40.8164 & dataframe$pickup_latitude >= 40.8029),]
@@ -24,50 +28,35 @@ dataframe <- mutate(dataframe, pickup_res = hour(res)*60 + minute(res))
 dataframe <- mutate(dataframe, dropoff_res = hour(res2)*60 + minute(res2))
 dataframe <- dataframe %>%
   mutate(total_time = ifelse(pickup_res < dropoff_res, dropoff_res - pickup_res, pickup_res - dropoff_res))
-dataframe <- dataframe[, c(-22:-23)]
-dataframe2 <- dataframe
-dataframe2 <- dataframe2 %>%
+dataframe <- dataframe %>%
   mutate(store_and_fwd_flag = ifelse(store_and_fwd_flag == 'N', 0, 1))
-df_mins <- dataframe2
-df_mins <- df_mins[, c(-1:-2, -4:-5)]
-df_hrs <- df_mins
-dataframe2 <- dataframe2[, c(-1:-5)]
-write.csv(dataframe2, 
-          file = "C:/Users/abhishek.suntwal/Downloads/no_datetime.csv", 
-          sep = ",",
-          na = "NA", 
-          row.names = FALSE, 
-          col.names = FALSE, 
-          eol = "\n", 
-          append = FALSE,
-          quote = FALSE)
+dataframe4 <- dataframe[, c(-22:-23)]
+seq <- seq(0, 150, 10)
+dataframe4$amount_bins <- as.numeric(cut2(dataframe4$total_amount, seq))
+table(dataframe4$amount_bins)
+plot(dataframe4$amount_bins)
+summary(dataframe4$total_amount)
+dataframe3 <- dataframe4[, c(-2:-5,-15:-21)]
+dataframe4 <- dataframe4[, c(-2:-5, -21)]
 
-
-
-res <- hms(df_mins$pickup_time)
-df_mins <- mutate(df_mins, pickup_time = hour(res)*60 + minute(res))
-write.csv(df_mins, 
-          file = "C:/Users/Abhishek/Documents/GitHub/NYC-College-Taxi/pickup_in_mins.csv", 
+write.csv(dataframe3, 
+          file = "C:/Users/abhishek.suntwal/Downloads/pred_amt_with_all_columns.csv", 
           sep = ",",
           na = "NA", 
           row.names = FALSE, 
           col.names = FALSE, 
           eol = "\n", 
           append = TRUE,
-          quote = FALSE)
+          quote = FALSE) 
 
-df_hrs <- mutate(df_hrs, pickup_time = hour(res))
-
-write.csv(df_hrs, 
-          file = "C:/Users/Abhishek/Documents/GitHub/NYC-College-Taxi/pickup_in_hrs.csv", 
+write.csv(dataframe3, 
+          file = "C:/Users/abhishek.suntwal/Downloads/pred_amt_with_few_columns.csv", 
           sep = ",",
           na = "NA", 
           row.names = FALSE, 
           col.names = FALSE, 
           eol = "\n", 
           append = TRUE,
-          quote = FALSE)
+          quote = FALSE) 
 
-
-sum(is.na(dataframe2))
 
